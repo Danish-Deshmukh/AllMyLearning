96d21dc8e6767168f285a690868da1ec7137cdd8
danish <dd3977@gmail.com>
2023-04-24 06:09:43 +0530

    24/04/2023

--- TimSir/src/section16Collections/_04SortedCollection/StockItem.java
+++ TimSir/src/section16Collections/_04SortedCollection/StockItem.java
@@ -1,4 +1,4 @@
-package section16Collections._04SortedCollections;
+package section16Collections._04SortedCollection;
 
 public class StockItem implements Comparable<StockItem> {
     private final String name;
